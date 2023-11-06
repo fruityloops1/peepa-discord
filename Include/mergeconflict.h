0cf53cc898622b0a5ff8d11b1cdc2d99991f8159
Fruityloops <73552844+fruityloops1@users.noreply.github.com>
2023-11-06 09:25:22 +0000

    maybe

--- Include/mergeconflict.h
+++ Include/mergeconflict.h
@@ -7,14 +7,14 @@ constexpr const char sConflictFmt1[] = R"(
   <title>Solve Merge Conflict</title>
   <script>
     function selectOldButtons() {
-      var oldButtons = document.querySelectorAll('input[value="old"]');
+      var oldButtons = document.querySelectorAll('input[name^="conflict1-"]');
       for (var i = 0; i < oldButtons.length; i++) {
         oldButtons[i].checked = true;
       }
     }
     
     function selectNewButtons() {
-      var newButtons = document.querySelectorAll('input[value="new"]');
+      var newButtons = document.querySelectorAll('input[name^="conflict2-"]');
       for (var i = 0; i < newButtons.length; i++) {
         newButtons[i].checked = true;
       }
@@ -65,8 +65,8 @@ constexpr const char sConflictFmt1[] = R"(
   <h2>Solve Merge Conflict</h2>
   <p>Merge conflicts have been found. Please select the symbols of higher quality and click submit.</p>
   
-  <button onclick="selectOldButtons()\">Select all old</button>
-  <button onclick="selectNewButtons()\">Select all new</button>
+  <button onclick="selectOldButtons();">Select all old</button>
+  <button onclick="selectNewButtons();">Select all new</button>
   <br><br>
   <form method="post" action="submit_resolution">
     <table>
